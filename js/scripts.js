dc9aabf3faa79770c619b2cf53ca2ecd65c54615
Charles Mwathi <mwathi.kinyua@gmail.com>
2021-05-25 06:25:15 +0300

    Add a to do list items constructor.

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+// Business Logic
+
+function toDoListItems(date, timeSpan, task, taskDescription) {
+  this.date = date;
+  this.timeSpan = timeSpan;
+  this.task = task;
+  this.taskDescription = taskDescription;
+}
