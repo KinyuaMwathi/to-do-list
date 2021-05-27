4f3dd1ee3c5e5996483d9a5225cebaafe0184ac6
Charles Mwathi <mwathi.kinyua@gmail.com>
2021-05-27 06:32:57 +0300

    Add a to do list items collection constructor.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,21 @@
 // Business Logic
 
-function toDoListItems(date, timeSpan, task, taskDescription) {
+function ToDoListItemsCollection() {
+  this.toDoListItems = {};
+  this.currentId = 0;
+}
+
+ToDoListItemsCollection.prototype.addToDoListItems = function(toDoListItems) {
+  toDoListItems.id = this.assignId();
+  this.toDoListItems[toDoListItems.id] = toDoListItems;
+}
+
+ToDoListItemsCollection.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+}
+
+function ToDoListItems(date, timeSpan, task, taskDescription) {
   this.date = date;
   this.timeSpan = timeSpan;
   this.task = task;
